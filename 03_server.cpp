a8b1002d33018ee0a7471e0bcacc725807aa45d7
AndrewMarcondes <drewmarcondes@gmail.com>
2025-08-19 05:11:18 -0400

    Client and Server

---
+++ 03_server.cpp
@@ -0,0 +1,75 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+// Forward declaration
+static void do_something(int connfd);
+
+// Error handling functions
+void die(const char* msg) {
+	perror(msg);
+	exit(1);
+}
+
+void msg(const char* message) {
+	fprintf(stderr, "%s\n", message);
+}
+
+
+int main() {
+
+
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	int val = 1;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+	struct sockaddr_in addr = {};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(1234);		// port
+	addr.sin_addr.s_addr = htonl(0); 	// wildcard IP 0.0.0.0
+	int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+	if (rv) { die("bind()"); }
+
+	// listen
+	rv = listen(fd, SOMAXCONN);
+	if (rv) { die("listen()"); }
+
+	while (true) {
+		//accept
+		struct sockaddr_in client_addr = {};
+		socklen_t addrlen = sizeof(client_addr);
+		int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+		if (connfd < 0) {
+			continue;	// error
+		}
+
+		do_something(connfd);
+		close(connfd);
+	}
+
+	close(fd);
+	
+	return 0;
+
+}
+
+
+
+static void do_something(int connfd) {
+	char rbuf[64] = {};
+	ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+	if (n < 0) {
+		msg("read() error");
+		return;
+	}
+	printf("client says %s\n", rbuf);
+
+	char wbuf[] = "world";
+	write(connfd, wbuf, strlen(wbuf));
+}
